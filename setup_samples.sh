c479c68bf4e679aac84f807e29cf5fa2ddd98da7
fleth <fleth@users.noreply.github.com>
2019-04-21 08:10:15 +0000

    update setup

--- setup_samples.sh
+++ setup_samples.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-current=`pwd`
-echo $current
 git submodule update -i
 git submodule foreach git pull origin master
 
-mkdir ~/workspace
-rm ~/workspace/stock_data
-ln -s $current/data ~/workspace/stock_data
-
 sh setup/install.sh
-sh setup/setup.sh
+sh setup/setup_sample.sh
